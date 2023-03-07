f7dadc3b0bf34606932b4b895cc9692249ccd10a
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2023-03-07 23:28:06 +0100

    Remove dummy files

--- src/dummy.cpp
+++
@@ -1,7 +0,0 @@
-#include "dummy.hpp"
-
-namespace mp {
-int dummy() {
-    return 0;
-}
-} // namespace mp
--- src/dummy.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef MEMPOOL_DUMMY_HPP
-#define MEMPOOL_DUMMY_HPP
-
-namespace mp {
-int dummy();
-}
-
-#endif//MEMPOOL_DUMMY_HPP
