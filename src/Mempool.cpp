edee7d7e87316b1abf5590bd02f7c949c5108d28
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2023-03-07 23:25:43 +0100

    Add integration tests and fix bugs

--- src/Mempool.cpp
+++ src/Mempool.cpp
@@ -25,11 +25,20 @@ Mempool::Mempool(MempoolConfig config, std::size_t alignment) :
     std::sort(metadata_.config.begin(),
               metadata_.config.end(),
               [](const auto& lhs, const auto& rhs) { return lhs.chunk_size < rhs.chunk_size; });
+    if (std::any_of(
+            metadata_.config.cbegin(),
+            metadata_.config.cend(),
+            [alignment](const auto& sub_pool) { return sub_pool.chunk_size % alignment; })) {
+        auto msg = fmt::format(
+            "Provided chunk sizes (config: '{}') are not multiples of alignment '{}'",
+            metadata_.config,
+            alignment);
+        LOG_AND_THROW(MempoolExcetption, msg);
+    }
     auto size = GetTotalSize(metadata_.config);
 
     metadata_.chunk_size_to_offset_map = GetChunkSizeToOffsetMap(metadata_.config);
-    LOG(size.chunks);
-    metadata_.allocated_chunks.resize(size.chunks);
+    metadata_.allocated_chunks.resize(static_cast<std::size_t>(size.chunks));
 
     memory_.size = size;
     if (memory_.size.bytes > total_limit) {
@@ -53,33 +62,43 @@ void* Mempool::alloc(std::size_t size) noexcept {
         metadata_.config.cend(),
         [size](const auto& sub_pool) { return size <= sub_pool.chunk_size; });
     auto offset = metadata_.chunk_size_to_offset_map[sub_pool->chunk_size];
-    auto sub_pool_begin = metadata_.allocated_chunks.begin() + offset.index;
-    auto sub_pool_end = sub_pool_begin + sub_pool->chunk_amount;
 
     std::lock_guard lock(mutex_);
+    auto sub_pool_begin = metadata_.allocated_chunks.begin() + offset.index;
+    auto sub_pool_end = sub_pool_begin + sub_pool->chunk_amount;
     auto chunk = std::find(sub_pool_begin, sub_pool_end, false);
-    if (chunk == metadata_.allocated_chunks.end()) {
+    if (chunk == sub_pool_end) {
         return nullptr;
     }
+
     *chunk = true;
-    auto chunk_idx = static_cast<std::size_t>(std::distance(sub_pool_begin, chunk));
-    LOG("chunk_idx: " + std::to_string(chunk_idx));
-    auto ret = offset.address + (chunk_idx * sub_pool->chunk_size);
-    LOG("ret: " + std::to_string(ret));
-    return memory_.data.get() + ret;
+    auto chunk_index = static_cast<std::size_t>(std::distance(sub_pool_begin, chunk));
+    auto chunk_address = offset.address + (chunk_index * sub_pool->chunk_size);
+    return memory_.data.get() + chunk_address;
 }
 
 void* Mempool::free(void* p) noexcept {
     if (p < memory_.data.get() or p > memory_.data.get() + memory_.size.bytes) {
         return nullptr;
     }
-    auto chunk = static_cast<std::size_t>(static_cast<std::byte*>(p) - memory_.data.get());
-    if (static_cast<std::size_t>(chunk) % memory_.alignment) {
+    auto chunk_address = static_cast<std::size_t>(static_cast<std::byte*>(p) - memory_.data.get());
+    if (static_cast<std::size_t>(chunk_address) % memory_.alignment) {
         return nullptr;
     }
     std::lock_guard lock(mutex_);
-    metadata_.allocated_chunks[chunk] = false;
-    return memory_.data.get() + chunk;
+    for (auto sub_pool = metadata_.config.rbegin(); sub_pool != metadata_.config.rend();
+         ++sub_pool) {
+        if (auto offset = metadata_.chunk_size_to_offset_map[sub_pool->chunk_size];
+            offset.address <= chunk_address) {
+            auto chunk_index = static_cast<std::size_t>(offset.index) +
+                               (chunk_address - offset.address) / sub_pool->chunk_size;
+            metadata_.allocated_chunks[chunk_index] = false;
+            return p;
+        }
+    }
+    // Should not end up here. I'm too tired to provide some real-time logging system.
+    // Maybe spdlog would meet the requirements.
+    return nullptr;
 }
 
 } // namespace mp
--- test/Mempool.cpp
+++ test/Mempool.cpp
@@ -1,16 +1,34 @@
 #include "Mempool.hpp"
 
+#include <numeric>
+
 #include <gtest/gtest.h>
 
 namespace mp::ut {
+
+namespace {
+std::int64_t GetPtrDiff(void* lhs, void* rhs) {
+    return static_cast<std::byte*>(lhs) - static_cast<std::byte*>(rhs);
+}
+
+template<typename T>
+T GetSecondToLastElement(const std::vector<T>& vec) {
+    return vec.rbegin()[1];
+}
+} // namespace
 
 TEST(MempoolInitialization, ConstructsPoolWithValidAlignment) {
     EXPECT_NO_THROW({ Mempool mempool({}, 64); });
 }
 
 TEST(MempoolInitialization, ThrowsOnTooSmallAlignment) {
-    MempoolConfig config{{1024, 1024}, {1024, 2048}};
+    MempoolConfig config{{1024, 1024}, {1024, 512}};
     EXPECT_THROW({ Mempool mempool(config, 0); }, MempoolExcetption);
+}
+
+TEST(MempoolInitialization, ThrowsOnTooChunkSizeNotBeinMultipleOfAlignment) {
+    MempoolConfig config{{1024, 1024}, {1024, 255}};
+    EXPECT_THROW({ Mempool mempool(config, 64); }, MempoolExcetption);
 }
 
 TEST(MempoolInitialization, ConstructsPoolWithValidSize) {
@@ -31,11 +49,87 @@ TEST(MempoolAllocation, AllocsValidSize) {
     });
 }
 
+TEST(MempoolAllocation, AllocatedMemoryCanBeAssigned) {
+    MempoolConfig config{{1024, 1024}, {1024, 64}};
+    EXPECT_NO_THROW({
+        Mempool mempool(config, 64);
+        auto ptr = static_cast<std::int32_t*>(mempool.alloc(64));
+        std::iota(ptr, ptr + 16, 0);
+        for (std::int32_t i{}; i < 16; i++) {
+            EXPECT_EQ(ptr[i], i);
+        }
+    });
+}
+
 TEST(MempoolAllocation, ReturnsNullptrOnSizeBiggerThanTheBiggestChunk) {
     MempoolConfig config{{1024, 1024}, {1024, 512}};
     EXPECT_NO_THROW({
         Mempool mempool(config, 64);
         EXPECT_EQ(mempool.alloc(2048), nullptr);
+    });
+}
+
+TEST(MempoolAllocation, ReturnsNullptrOnNoChunksLeftInSubPool) {
+    MempoolConfig config{{1024, 1024}, {2, 2048}};
+    EXPECT_NO_THROW({
+        Mempool mempool(config, 64);
+        EXPECT_NE(mempool.alloc(1500), nullptr);
+        EXPECT_NE(mempool.alloc(1700), nullptr);
+        EXPECT_EQ(mempool.alloc(1500), nullptr);
+    });
+}
+
+TEST(MempoolAllocation, ReturnsProperlyIncrementedPointer) {
+    MempoolConfig config{{32, 1024}, {256, 512}};
+    EXPECT_NO_THROW({
+        std::vector<void*> ptrs;
+        ptrs.reserve(32);
+        Mempool mempool(config, 64);
+
+        ptrs.push_back(mempool.alloc(666));
+        ASSERT_NE(ptrs.back(), nullptr);
+
+        ptrs.push_back(mempool.alloc(777));
+        EXPECT_EQ(GetPtrDiff(ptrs.back(), ptrs.front()), 1024);
+
+        ptrs.push_back(mempool.alloc(888));
+        EXPECT_EQ(GetPtrDiff(ptrs.back(), GetSecondToLastElement(ptrs)), 1024);
+
+        ptrs.push_back(mempool.alloc(420));
+        ASSERT_NE(ptrs.back(), nullptr);
+
+        ptrs.push_back(mempool.alloc(200));
+        EXPECT_EQ(GetPtrDiff(ptrs.back(), GetSecondToLastElement(ptrs)), 512);
+
+        EXPECT_EQ(GetPtrDiff(ptrs.front(), GetSecondToLastElement(ptrs)), 256 * 512);
+    });
+}
+
+TEST(MempoolAllocation, DenselyAllocatedSubPoolsDoNotInterleave) {
+    MempoolConfig config{{2, 1024}, {2, 512}};
+    EXPECT_NO_THROW({
+        std::vector<void*> ptrs;
+        ptrs.reserve(32);
+        Mempool mempool(config, 64);
+
+        ptrs.push_back(mempool.alloc(666));
+        ASSERT_NE(ptrs.back(), nullptr);
+
+        ptrs.push_back(mempool.alloc(777));
+        EXPECT_EQ(GetPtrDiff(ptrs.back(), ptrs.front()), 1024);
+
+        EXPECT_EQ(mempool.alloc(777), nullptr);
+
+        ptrs.push_back(mempool.alloc(420));
+        ASSERT_NE(ptrs.back(), nullptr);
+
+        ptrs.push_back(mempool.alloc(200));
+        EXPECT_EQ(GetPtrDiff(ptrs.back(), GetSecondToLastElement(ptrs)), 512);
+
+        EXPECT_EQ(mempool.alloc(42), nullptr);
+
+        EXPECT_EQ(GetPtrDiff(ptrs.front(), GetSecondToLastElement(ptrs)), 2 * 512);
+        EXPECT_EQ(GetPtrDiff(ptrs[0], ptrs[3]), 512);
     });
 }
 
@@ -48,7 +142,7 @@ TEST(MempoolFree, FreesValidPointer) {
     });
 }
 
-TEST(MempoolFree, ReturnsNullptrOnSizeBiggerThanTheBiggestChunk) {
+TEST(MempoolFree, ReturnsNullptrOnNotAllocatedAddress) {
     MempoolConfig config{{1024, 1024}, {1024, 512}};
     EXPECT_NO_THROW({
         Mempool mempool(config, 64);
@@ -56,5 +150,33 @@ TEST(MempoolFree, ReturnsNullptrOnSizeBiggerThanTheBiggestChunk) {
         auto invalid_ptr = static_cast<std::byte*>(ptr) - 10;
         EXPECT_EQ(mempool.free(invalid_ptr), nullptr);
     });
+}
+
+TEST(MempoolFree, ReturnsNullptrOnNotAlignedAddress) {
+    MempoolConfig config{{1024, 1024}, {1024, 512}};
+    EXPECT_NO_THROW({
+        Mempool mempool(config, 64);
+        auto ptr = mempool.alloc(512);
+        auto invalid_ptr = static_cast<std::byte*>(ptr) + 10;
+        EXPECT_EQ(mempool.free(invalid_ptr), nullptr);
+    });
+}
+
+TEST(MempoolAllocAndFree, ProperlyKeepsTrackOfAllocatedIndices) {
+    MempoolConfig config{{4, 1024}, {4, 512}};
+    //    EXPECT_NO_THROW({
+    std::vector<void*> ptrs;
+    ptrs.reserve(32);
+    Mempool mempool(config, 64);
+
+    ptrs.push_back(mempool.alloc(666));
+    ASSERT_NE(ptrs.back(), nullptr);
+
+    ptrs.push_back(mempool.alloc(777));
+    ptrs.push_back(mempool.alloc(888));
+    ptrs.push_back(mempool.free(ptrs[1]));
+    ptrs.push_back(mempool.alloc(999));
+    EXPECT_EQ(ptrs.back(), GetSecondToLastElement(ptrs));
+    //    });
 }
 } // namespace mp::ut
