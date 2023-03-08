c05ee88e731019e97a74d1b18b40a968ee480a92
Szymon Zosgornik <szymon.zosgornik@gmail.com>
2023-03-08 02:05:41 +0100

    Enable -Werror and do minor cleanup

--- src/Config.cpp
+++ src/Config.cpp
@@ -5,15 +5,15 @@
 namespace mp {
 
 MempoolSize GetTotalSize(const MempoolConfig& config) {
-    return std::accumulate(config.cbegin(),
-                           config.cend(),
-                           MempoolSize{},
-                           [](MempoolSize sum, const auto& sub_pool) -> MempoolSize {
-                               return {.chunks = sum.chunks + sub_pool.chunk_amount,
-                                       .bytes = sum.bytes +
-                                                (static_cast<std::size_t>(sub_pool.chunk_amount) *
-                                                 sub_pool.chunk_size)};
-                           });
+    return std::accumulate(
+        config.cbegin(),
+        config.cend(),
+        MempoolSize{},
+        [](MempoolSize sum, const auto& sub_pool) -> MempoolSize {
+            return {sum.chunks + sub_pool.chunk_amount,
+                    sum.bytes +
+                        (static_cast<std::size_t>(sub_pool.chunk_amount) * sub_pool.chunk_size)};
+        });
 }
 
 ChunkSizeToOffsetMap GetChunkSizeToOffsetMap(const MempoolConfig& config) {
--- test/Config.cpp
+++ test/Config.cpp
@@ -8,12 +8,14 @@ TEST(MempoolConfig, GetTotalSizeTest1) {
     MempoolConfig config{{1024, 1024}, {1024, 2048}};
     auto size = GetTotalSize(config);
     EXPECT_EQ(size.bytes, 1024 * 1024 + 1024 * 2048);
+    EXPECT_EQ(size.chunks, 1024 + 1024);
 }
 
 TEST(MempoolConfig, GetTotalSizeTest2) {
-    MempoolConfig config{{1024, 1024}, {1024, 512}};
+    MempoolConfig config{{1024, 1024}, {2048, 512}};
     auto size = GetTotalSize(config);
-    EXPECT_EQ(size.bytes, 1024 * 1024 + 1024 * 512);
+    EXPECT_EQ(size.bytes, 1024 * 1024 + 2048 * 512);
+    EXPECT_EQ(size.chunks, 1024 + 2048);
 }
 
 } // namespace mp::ut
